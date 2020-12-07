8460c11dd0c7d57e6709c75313310858dc058f00
Chelsea Becker <cschweig2@gmail.com>
2020-12-07 14:17:55 -0800

    add CanQueenAttack function and UI
    Co-authored-by: Ellie Lambert <ebezjian@gmail.com>

---
+++ Logic/QueenAttack.cs
@@ -0,0 +1,26 @@
+namespace QueenAttack.Logic
+{
+  public class Queen
+  {
+    public int XCoordinate { get; set; }
+    public int YCoordinate { get; set; }
+
+    public Queen(int xCoordinate, int yCoordinate)
+    {
+      XCoordinate = xCoordinate;
+      YCoordinate = yCoordinate;
+    }
+
+    public static bool CanQueenAttack(int x, int y)
+    {
+      if (x == 4 || y == 1)
+      {
+        return true;
+      }
+      else
+      {
+        return false;
+      }
+    }
+  }
+}
--- Program.cs
+++ Program.cs
@@ -1,14 +1,37 @@
 using System;
 using System.Collections.Generic;
+using QueenAttack.Logic;
 
-
-namespace Attack
+namespace QueenAttack
 {
   public class Program
   {
+    public static void Main()
+    {
+      Queen whiteQueen = new Queen(4,1);
+      Console.WriteLine("Welcome to the game of chess. Please enter your piece location at coordinate X: ");
+      string strXCoord = Console.ReadLine();
+      int xCoord = int.Parse(strXCoord);
+      Console.WriteLine("Please enter your piece location at coordinate Y: ");
+      string strYCoord = Console.ReadLine();
+      int yCoord = int.Parse(strYCoord);
 
+      bool attack = Queen.CanQueenAttack(xCoord, yCoord);
 
-
-
+      if (attack)
+      {
+        Console.WriteLine("The queen has won");
+      } 
+      else 
+      {
+        Console.WriteLine("You have bested the queen");
+      }
+    }
   }
 }
+// chessboard is 8x8
+// user submits coordinates
+// the program creates an instance of Queen class with X and Y coordinates
+// the Queen class has a method that takes the user's coordinates as argument
+// the method will return true if the queen can attack 
+// the user should see the result displayed
